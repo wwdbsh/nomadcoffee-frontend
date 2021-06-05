c8d0540b67646d37d65e0a66a76adb3f9c73fb32
Sangheon Lee <wwdbsh@gmail.com>
2021-06-05 14:35:55 -0400

    20210605:nomad-coffee-front redirect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useReactiveVar } from "@apollo/client";
 import { useState } from "react";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Redirect, Route, Switch } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import { isLoggedInVar, darkModeVar } from "./apollo";
 import Home from "./screens/Home";
@@ -23,6 +23,7 @@ function App() {
               <Login />
             )}
           </Route>
+          {/* <Redirect path="/"/> */}
           <Route>
             <NotFound/>
           </Route>
